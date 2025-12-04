2abaa8cd29698b4176f973cc88738757e0c123cb
ignaciorodriguezbeis <igna173arg@gmail.com>
2025-12-04 02:23:45 -0300

    modified:   tp-React-coderhouse/src/App.jsx

--- tp-React-coderhouse/src/App.jsx
+++ tp-React-coderhouse/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar/Navbar'
 import ItemDetail from './components/Container/ItemDetairl'    //una cosa curiosa que me paso enesta linea es que si ponia ItemDetail me tiraba error, tuve que poner ItemDetairl como en el nombre del archivo
 import Footer from './components/Footer/Footer'
